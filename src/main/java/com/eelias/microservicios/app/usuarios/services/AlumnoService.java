9add16f652aad053094a44d472a8c32967195ff7
Emmanuel Elias <emmanuelelias@Emmanuels-MacBook-Air.local>
2021-09-24 17:41:33 -0500

    Agregando clases servicio y repositorio al proyecto

--- src/main/java/com/eelias/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/eelias/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,5 +1,18 @@
 package com.eelias.microservicios.app.usuarios.services;
 
+import java.util.Optional;
+
+import com.eelias.microservicios.app.usuarios.models.entity.Alumno;
+
 public interface AlumnoService {
 
+	public Iterable<Alumno> findAll();
+		
+	
+	public Optional<Alumno> findById(Long id);
+	
+	public Alumno save(Alumno alumno);
+	
+	public void deleteById(Long id);
+	
 }
